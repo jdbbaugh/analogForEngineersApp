d2e2bf766cb2af562d269a13fa365c1e2e12909f
Joseph <jbaugh78@msn.com>
2019-05-01 10:55:57 -0500

    messing with applicationDbContext.cs now

--- analogCapstone/Data/ApplicationDbContext.cs
+++ analogCapstone/Data/ApplicationDbContext.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using analogCapstone.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace analogCapstone.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+            : base(options) { }
+            public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+            public DbSet<Song> Song { get; set; }
+            public DbSet<Channel> Channel { get; set; }
+            public DbSet<ChannelToGear> ChannelToGear { get; set; }
+            public DbSet<Gear> Gear { get; set; }
+            public DbSet<Knob> Knob { get; set; }
     }
 }

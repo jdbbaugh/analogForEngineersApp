5ae5d7cc25ae397bf1de5434433ecbb3139a38e0
Joseph <jbaugh78@msn.com>
2019-05-10 05:07:13 -0500

    channel listing for song specific seems to be good need to add ability to alter settings now

--- analogCapstone/Controllers/ChannelsController.cs
+++ analogCapstone/Controllers/ChannelsController.cs
@@ -88,6 +88,7 @@ namespace analogCapstone.Controllers
                             Setting = ks.KnobSetting
                         }).ToList()
                 }).ToListAsync();
+           
 
             model.ApplicationUser = user;
             model.Channel = getChannel;
